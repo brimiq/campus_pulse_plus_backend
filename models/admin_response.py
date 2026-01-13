f27e479f7c9d7557429630d90a942d17f2e3dab9
Hidaya Mohamed <hidayamohamed002@gmail.com>
2026-01-13 16:31:40 +0300

    Added columns to AdminResponse class

--- models/admin_response.py
+++ models/admin_response.py
@@ -2,4 +2,12 @@ from sqlalchemy.orm import relationship
 from models.user import db
 
 class AdminResponse(db.Model):
-    pass
+    __tablename__ = "admin_responses"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+
+    admin_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
