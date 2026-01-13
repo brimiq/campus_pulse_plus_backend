46c526b4c9edc2d75b4c42bdb81cce673e25620e
Hidaya Mohamed <hidayamohamed002@gmail.com>
2026-01-13 17:22:06 +0300

    Added a function to_dict to admin_response.py

--- models/admin_response.py
+++ models/admin_response.py
@@ -1,16 +1,17 @@
-from sqlalchemy.orm import relationship
-from models.user import db
+from config import db
 
 class AdminResponse(db.Model):
     __tablename__ = "admin_responses"
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
-
-    admin_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+    admin_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "post_id": self.post_id
+        }
 
-    # relationship
-    admin = relationship("User")
